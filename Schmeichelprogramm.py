305b37d962fc2707aa67eaa42bbe7a4f86ab8d37
jogi15 <xrasi@gmx.de>
2020-02-06 20:50:10 +0100

    Schmeichelprogramm

---
+++ Schmeichelprogramm.py
@@ -0,0 +1,6 @@
+import random
+adjektive = ["beste","liebenswürdigste","schönste","größte"]
+nomen = ["Mensch","Hecht","Freund","Kumpel","Programmierer"]
+print("Du bist der ")
+print(random.choice(adjektive))
+print(random.choice(nomen))
